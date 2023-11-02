a268b2b792ac5f450f957a2de920ffdcb7e8e374
IzumiShaka-desu <shakaaji29@gmail.com>
2023-11-02 21:58:30 +0700

    create app js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import Users from './pages/Users';
+import AddUser from './pages/AddAdmin';
+import { BrowserRouter as Router, NavLink, Link, createBrowserRouter, Route, RouterProvider } from 'react-router-dom';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/login",
+      element: (
+        <Login />
+      ),
+    },
+    {
+      path: "/",
+      element: (
+        <div>
+          <Navbar />
+          <Dashboard />
+        </div>
+      ),
+    },
+    {
+      path: "/users",
+      element: (
+        <div>
+          <Navbar >
+            <Users />
+          </Navbar>
+
+        </div>
+      ),
+    },
+    {
+      path: "/users/add",
+      element: (
+        <div>
+          <Navbar >
+            <AddUser />
+          </Navbar>
+
+        </div>
+      ),
+    },
+    {
+      path: "about",
+      element: <div>About</div>,
+    },
+  ]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router} />
     </div>
   );
 }
